97517f260e3f57637f077569937434481b65d5a8
Bayron <bayron.valenzuela.g@gmail.com>
2023-08-25 21:03:35 -0400

    make calls to the db and restructure some code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from password_manager import PasswordManager
 
 if __name__ == "__main__":
     db_class = DbOperations()
-    db_class.delete_table()
     password_manager = PasswordManager(db_class)
-    password_manager.ir_a_menu()
+    password_manager.menu()
 
--- password_manager.py
+++ password_manager.py
@@ -3,20 +3,8 @@ import os
 
 class PasswordManager:
     def __init__(self, db_class):
-        self.db_class = db_class
-        self.db_class.create_table()
-
-    def ir_a_menu(self):
-        # esto es para que no tengamos que ir al menu a cada que ejecutamos el programa
-        # borrar cuando ya estemos terminando
-        ir = input("Desea ir al menu? (y/n): ")
-        match ir:
-            case "y":
-                self.menu()
-            case "n":
-                print("Exiting...")
-            case _:
-                print("Invalid choice")
+        self.db = db_class
+        self.db.create_table()
 
     def clear_screen(self):
         os.system("cls" if os.name == "nt" else "clear")
@@ -25,62 +13,77 @@ class PasswordManager:
         self.clear_screen()
         print("Password Manager")
         print("----------------")
-        print("1. Create new password")
+        print("1. Show all passwords")
         print("2. View password")
         print("3. Delete password")
-        print("4. Show all passwords")
+        print("4. Create new password")
         print("5. Update password")
         print("6. Exit")
         print("----------------")
         print("Enter your choice: ", end="")
 
-    def create_password(self):
+    def encrypt(self, password):
         pass
 
-    def view_password(self):
+    def check_password(self, password):
+        # TODO: Check if password meets basic criteria
         pass
 
-    def delete_password(self):
-        pass
+    def create_password(self, website, username, password):
+        # TODO: encrypt password here, new function
+        self.db.insert_password(website, username, password)
+
+    def view_password(self, website, username):
+        self.db.view_password(website, username)
+
+    def delete_password(self, website, username): # add password?
+        self.db.delete_password(website, username)
 
     def show_all_passwords(self):
-        pass
+        self.db.show_all_passwords()
 
-    def update_password(self):
-        pass
+    def update_password(self, website, username, password, newpassword):
+        #TODO: encrypt password here, new function
+        self.db.modify_password(website, username, password, newpassword) #TODO: add newpassword in db_operations
+
+    def action(self, choice):
+        if choice == 1:
+            self.show_all_passwords()
+        elif choice == 2:
+            website = input("Enter website: ")
+            username = input("Enter username: ")
+            self.view_password(website, username)
+        elif choice == 3:
+            website = input("Enter website: ")
+            username = input("Enter username: ")
+            self.delete_password(website, username)
+        elif choice == 4:
+            website = input("Enter website: ")
+            username = input("Enter username: ")
+            password = input("Enter password: ")
+            self.create_password(website, username, password)
+        elif choice == 5:
+            website = input("Enter website: ")
+            username = input("Enter username: ")
+            password = input("Enter current password: ")
+            newpassword = input("Enter new password: ")
+            self.update_password(website, username, password, newpassword)
+        else:
+            print("Invalid choice")
+
+        return True
+
 
     def menu(self):
-        while True:
+        flag = True
+        while flag:
             self.print_menu()
             choice = int(input())
 
-            if choice == 1:
-                try:
-                    self.create_password()
-                except Exception as e:
-                    print("Error creating password:", e)
-            elif choice == 2:
-                try:
-                    self.view_password()
-                except Exception as e:
-                    print("Error viewing password:", e)
-            elif choice == 3:
-                try:
-                    self.delete_password()
-                except Exception as e:
-                    print("Error deleting password:", e)
-            elif choice == 4:
-                try:
-                    self.show_all_passwords()
-                except Exception as e:
-                    print("Error showing all passwords:", e)
-            elif choice == 5:
-                try:
-                    self.update_password()
-                except Exception as e:
-                    print("Error updating password:", e)
-            elif choice == 6:
-                print("Exiting...")
+            if choice == 6:
                 break
-            else:
-                print("Invalid choice")
+
+            try:
+                flag = self.action(choice)
+            except:
+                print(f"Error in {choice}")
