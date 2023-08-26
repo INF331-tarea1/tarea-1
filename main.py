e05d313daf5b28e61cc6e092e938de395d99a73c
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-08-26 17:00:46 -0400

    Fix dummy checking with master password
    Changes:
    - The generate_encryption_key() method was deleted and defined as a function in main.
    - The internal variable key is now passed as a parameter.
    - Now the connection to the database is closed when exiting the program.
    - Fixed the dummy verification error with the master key.

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 from db_operations import DbOperations
 from password_manager import PasswordManager
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import serialization
+from cryptography.fernet import Fernet
+import base64
+import os
 
+def generate_encryption_key():
+    path_variable = os.environ.get("PATH")
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=os.urandom(16),
+        iterations=480000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(path_variable.encode()))
+    return key
+
+def encrypt_password(key, password):
+    fernet = Fernet(key)
+    encrypted_password = fernet.encrypt(password.encode())
+    return encrypted_password
+
+def decrypt_password(key, encrypted_password):
+    fernet = Fernet(key)
+    decrypted_password = fernet.decrypt(encrypted_password).decode()
+    return decrypted_password
 
 if __name__ == "__main__":
     incorrect_master_password = True
-    db_class = DbOperations()
 
     while incorrect_master_password:
         MASTER_PASS = input("Enter the master password: ")
-        
-        password_manager = PasswordManager(db_class, MASTER_PASS)
-        all_passwords = db_class.show_all_passwords()
 
         
-        if all_passwords == -1:
-            password_manager.create_password("dummy", "dummy", "dummy")
+        if not os.path.exists("database.db"):
+            print("Database does not exist")
+            key = generate_encryption_key()
+            MASTER_PASS_ENCRYPT = encrypt_password(key, MASTER_PASS)
+            db_class = DbOperations()
+            db_class.insert_password("dummy_key", "dummy_key", key)
+            db_class.insert_password("dummy_pass", "dummy_pas", MASTER_PASS_ENCRYPT)
+            print(f"key: {key}")
+            print(f"master_pass_encrypt: {MASTER_PASS_ENCRYPT}")
             incorrect_master_password = False
         else:
-            try:
-                stored_dummy = password_manager.view_password('dummy', 'dummy')
+            print("Database exists")
+            db_class = DbOperations()
+            key = db_class.view_password("dummy_key", "dummy_key")[3]
+            MASTER_PASS_ENCRYPT = db_class.view_password("dummy_pass", "dummy_pas")[3]
+            MASTER_PASS_DECRYPT = decrypt_password(key, MASTER_PASS_ENCRYPT)
+            if MASTER_PASS == MASTER_PASS_DECRYPT:
+                print("Master password correct")
                 incorrect_master_password = False
-            except Exception as e:
-                print("Invalid master password.")
-                print(e)
-                print("Try again")
+            else:
+                print("Master password incorrect")
+                while incorrect_master_password:
+                    MASTER_PASS = input("Enter the master password: ")
+                    if MASTER_PASS == MASTER_PASS_DECRYPT:
+                        print("Master password correct")
+                        incorrect_master_password = False
+                    else:
+                        print("Master password incorrect")
 
-
+    password_manager = PasswordManager(db_class, MASTER_PASS, key)
     password_manager.menu()
